a313a0a4e140b419f31a1da63ee9778c27f26ed9
myaei <myaeimyo@gmail.com>
2018-06-18 00:37:41 +0630

    Add error event class

---
+++ app/src/main/java/com/mem/tedtalks/events/ApiErrorEvent.java
@@ -0,0 +1,14 @@
+package com.mem.tedtalks.events;
+
+public class ApiErrorEvent {
+
+    private String errorMsg;
+
+    public ApiErrorEvent(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+}
